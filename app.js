23d914eb78af03809020085be8bd401b4ead561f
Dominik Wędzina <dominik.wedzina@onet.pl>
2025-01-09 18:32:40 +0100

    change app.js file

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send("Hello World");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const PORT = 8080;
 
 app.listen(PORT, () => {
-console.log("Server is running...");
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
